2db0a06a107a542938fa4a6f90883841492da3e1
1998ganteng <113190620+1998ganteng@users.noreply.github.com>
2022-09-09 21:31:31 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,46 @@
+#iwan ganteng
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/ganteng/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/ganteng
+
+clear
+
+mkdir ganteng
+
+clear
+
+cd ganteng
+
+clear
+
+git clone https://github.com/1998ganteng/ganteng.git
+
+clear
+
+cd ganteng
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
